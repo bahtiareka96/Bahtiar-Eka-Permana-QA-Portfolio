f3d840a2adfcdc4b958f67f020ce6a009bb9492d
bahtiar <bahtiareka96@gmail.com>
2025-11-26 09:33:12 +0700

    Adding Plyawright Login Test

--- 01_Ecommerce_Website_OpenCart/Automation_Testing/tests/login.spec.ts
+++ 01_Ecommerce_Website_OpenCart/Automation_Testing/tests/login.spec.ts
@@ -1,11 +1,36 @@
 import { test, expect } from '@playwright/test';
 
-test('Login with valid credentials', async ({ page }) => {
-  await page.goto('https://demo.opencart.com/');
-  await page.getByRole('link', { name: 'My Account' }).click();
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByLabel('E-Mail').fill('testuser@example.com');
-  await page.getByLabel('Password').fill('Password123');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page).toHaveURL(/account/);
-});
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await expect(page).toHaveTitle('Swag Labs')
+})
+
+test('Login using correct user', async ({ page }) => {
+    await page.getByPlaceholder('Username').fill('standard_user')
+    await page.getByPlaceholder('password').fill('secret_sauce')
+    await page.locator('[data-test="login-button"]').click()
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+})
+
+test('Login using incorrect username', async ({ page }) => {
+    await page.getByPlaceholder('Username').fill('standard_failed')
+    await page.getByPlaceholder('password').fill('secret_sauce')
+    await page.locator('[data-test="login-button"]').click()
+    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service')
+})
+
+test('Login using incorrect password', async ({ page }) => {
+    await page.getByPlaceholder('Username').fill('standard_failed')
+    await page.getByPlaceholder('password').fill('secret_failed')
+    await page.locator('[data-test="login-button"]').click()
+    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service')
+})
+
+test('Logout', async ({ page }) => {
+    await page.getByPlaceholder('Username').fill('standard_user')
+    await page.getByPlaceholder('password').fill('secret_sauce')
+    await page.locator('[data-test="login-button"]').click()
+    await page.locator('//*[@id="react-burger-menu-btn"]').click()
+    await page.locator('//*[@id="logout_sidebar_link"]').click()
+    await expect(page).toHaveTitle('Swag Labs')
+})
